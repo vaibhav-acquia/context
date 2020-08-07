486494196dc6983ccc799a3b6d7e5024dd7dcdb7
paulocs <paulocs@3640109.no-reply.drupal.org>
2020-08-07 16:09:21 -0300

    Issue #3163057 by paulocs: Fix drupal code standard errors in context/src/Form

--- src/Form/AjaxFormTrait.php
+++ src/Form/AjaxFormTrait.php
@@ -6,8 +6,9 @@ use Drupal\Component\Serialization\Json;
 use Drupal\Component\Utility\NestedArray;
 
 /**
- * Provides helper methods for using an AJAX modal. This is a copy of the
- * ctools AjaxFormTrait.
+ * Provides helper methods for using an AJAX modal.
+ *
+ * This is a copy of the ctools AjaxFormTrait.
  */
 trait AjaxFormTrait {
 
@@ -15,6 +16,7 @@ trait AjaxFormTrait {
    * Gets attributes for use with an AJAX modal.
    *
    * @return array
+   *   Array with the ajax modal attributes.
    */
   public static function getAjaxAttributes() {
     return [
@@ -30,6 +32,7 @@ trait AjaxFormTrait {
    * Gets attributes for use with an add button AJAX modal.
    *
    * @return array
+   *   Array with the add button AJAX modal attributes.
    */
   public static function getAjaxButtonAttributes() {
     return NestedArray::mergeDeep(AjaxFormTrait::getAjaxAttributes(), [
