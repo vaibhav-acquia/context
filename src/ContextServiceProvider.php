634c801485fd64da8e59be0075019fc914842f7e
paulocs <paulocs@3640109.no-reply.drupal.org>
2020-09-14 14:03:55 -0300

    Issue #3018331 by paulocs, anzcar, Stephen Ollman, ydahi, bzoks, maticb: Install fails if module Menu Trail by Path is installed

--- src/ContextServiceProvider.php
+++ src/ContextServiceProvider.php
@@ -4,6 +4,7 @@ namespace Drupal\context;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\Core\Menu\MenuActiveTrail;
 
 /**
  * Alter the service container to use a custom class.
@@ -15,9 +16,12 @@ class ContextServiceProvider extends ServiceProviderBase {
    */
   public function alter(ContainerBuilder $container) {
     // Override the menu active trail with a new class.
-    $definition = $container->getDefinition('menu.active_trail');
-    $definition->setClass('Drupal\context\ContextMenuActiveTrail');
-    $definition->addArgument($container->getDefinition('context.manager'));
+    $definition = \Drupal::service('menu.active_trail');
+    if (!is_subclass_of($definition, 'Drupal\Core\Menu\MenuActiveTrail') && $definition instanceof MenuActiveTrail) {
+      $definition = $container->getDefinition('menu.active_trail');
+      $definition->setClass('Drupal\context\ContextMenuActiveTrail');
+      $definition->addArgument($container->getDefinition('context.manager'));
+    }
   }
 
 }
