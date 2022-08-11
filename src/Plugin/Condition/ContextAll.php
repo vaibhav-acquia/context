98e1d617796fee553f4bdfedbbef1d63c19cb32e
lpeabody <lpeabody@1137356.no-reply.drupal.org>
2022-08-11 14:55:59 +0000

    Issue #3295842 by lpeabody: Deleting a context which is negated in another context breaks the site

--- src/Plugin/Condition/ContextAll.php
+++ src/Plugin/Condition/ContextAll.php
@@ -146,9 +146,10 @@ class ContextAll extends ConditionPluginBase implements ContainerFactoryPluginIn
     // Handle negated contexts first.
     foreach ($negated_contexts as $name) {
       /** @var \Drupal\context\ContextInterface $negated_context */
-      $negated_context = $this->contextManager->getContext($name);
-      if ($this->contextManager->evaluateContextConditions($negated_context) && !$negated_context->disabled()) {
-        return FALSE;
+      if ($negated_context = $this->contextManager->getContext($name)) {
+        if ($this->contextManager->evaluateContextConditions($negated_context) && !$negated_context->disabled()) {
+          return FALSE;
+        }
       }
     }
 
