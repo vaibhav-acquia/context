55e0fbdab1b4465ad2aa437cfca37f10c20ff3d4
Lucas Cruz <lucassc@ciandt.com>
2022-09-08 17:30:57 -0300

    Replaced Block::loadMultiple with dependency injection

--- src/Reaction/Blocks/Form/BlockFormBase.php
+++ src/Reaction/Blocks/Form/BlockFormBase.php
@@ -26,6 +26,7 @@ use Drupal\Core\Render\Element\StatusMessages;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Provides a Block Form Base for blocks reactions.
@@ -112,6 +113,13 @@ abstract class BlockFormBase extends FormBase {
   protected $moduleHandler;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * Constructs a new VariantPluginFormBase.
    *
    * @param \Drupal\Component\Plugin\PluginManagerInterface $block_manager
@@ -130,6 +138,8 @@ abstract class BlockFormBase extends FormBase {
    *   The current request.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
    *   The module handler.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager service.
    */
   public function __construct(
     PluginManagerInterface $block_manager,
@@ -139,7 +149,8 @@ abstract class BlockFormBase extends FormBase {
     ContextReactionManager $contextReactionManager,
     ContextManager $contextManager,
     RequestStack $requestStack,
-    ModuleHandlerInterface $moduleHandler
+    ModuleHandlerInterface $moduleHandler,
+    EntityTypeManagerInterface $entityTypeManager
   ) {
     $this->blockManager = $block_manager;
     $this->contextRepository = $contextRepository;
@@ -149,6 +160,7 @@ abstract class BlockFormBase extends FormBase {
     $this->contextManager = $contextManager;
     $this->request = $requestStack->getCurrentRequest();
     $this->moduleHandler = $moduleHandler;
+    $this->entityTypeManager = $entityTypeManager;
   }
 
   /**
@@ -163,7 +175,8 @@ abstract class BlockFormBase extends FormBase {
       $container->get('plugin.manager.context_reaction'),
       $container->get('context.manager'),
       $container->get('request_stack'),
-      $container->get('module_handler')
+      $container->get('module_handler'),
+      $container->get('entity_type.manager'),
     );
   }
 
@@ -436,7 +449,7 @@ abstract class BlockFormBase extends FormBase {
   public function exists($id) {
 
     // Load all blocks.
-    $blocks = Block::loadMultiple();
+    $blocks = $this->entityTypeManager->getStorage('block')->loadMultiple();
 
     // Check if id of the block being added already exists.
     foreach ($blocks as $block) {
